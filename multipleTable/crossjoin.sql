271c6627d3d624c881e8d63a28b0bd32437f8090
Bathula <81623683+Bathula9@users.noreply.github.com>
2022-02-24 12:57:01 +0100

    Update crossjoin.sql

--- multipleTable/crossjoin.sql
+++ multipleTable/crossjoin.sql
@@ -131,3 +131,26 @@ GROUP BY month;
 Scroll down to see the final result.
 
 Which month has the highest subscribers? June!
+
+SELECT COUNT(*)
+FROM newspaper
+WHERE start_month <= 3 
+  AND end_month >= 3;
+  
+SELECT *
+FROM newspaper
+CROSS JOIN months;
+
+SELECT *
+FROM newspaper
+CROSS JOIN months
+WHERE start_month <= month 
+  AND end_month >= month;
+
+SELECT month, 
+  COUNT(*)
+FROM newspaper
+CROSS JOIN months
+WHERE start_month <= month 
+  AND end_month >= month
+GROUP BY month;
